fcc9e9ad04a48525115dd9765d380bfd90fbfa22
sadhana <nymphadoratonks112@gmail.com>
2026-07-23 12:31:41 +0530

    Add Spring Boot backend skeleton

---
+++ backend/src/test/java/com/college/noiseapp/urban_noise_system/UrbanNoiseSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.college.noiseapp.urban_noise_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanNoiseSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
